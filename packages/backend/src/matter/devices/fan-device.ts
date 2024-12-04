11d62b474f411f22499dc84509ae6abe7cce2086
Kenny Levinsen <kl@kl.wtf>
2024-12-04 23:25:04 +0100

    feat(fan-control): Implement fan control cluster
    This allows for fan speed, auto mode and direction control. The rocking
    and wind features are yet to be implemented.

--- packages/backend/src/matter/devices/fan-device.ts
+++ packages/backend/src/matter/devices/fan-device.ts
@@ -1,15 +1,17 @@
-import {
-  FanDeviceAttributes,
-  HomeAssistantEntityState,
-} from "@home-assistant-matter-hub/common";
-import { OnOffPlugInUnitDevice } from "@matter/main/devices";
+import { FanDevice as Device } from "@matter/main/devices";
 import { OnOffServer } from "../behaviors/on-off-server.js";
 import { BasicInformationServer } from "../behaviors/basic-information-server.js";
 import { IdentifyServer } from "../behaviors/identify-server.js";
+import { FanControlServer } from "../behaviors/fan-control-server.js";
 import {
   LevelControlConfig,
   LevelControlServer,
 } from "../behaviors/level-control-server.js";
+import {
+  FanDeviceAttributes,
+  FanDeviceFeature,
+  HomeAssistantEntityState,
+} from "@home-assistant-matter-hub/common";
 import { HomeAssistantEntityBehavior } from "../custom-behaviors/home-assistant-entity-behavior.js";
 import { EndpointType } from "@matter/main";
 
@@ -28,16 +30,34 @@ const fanLevelConfig: LevelControlConfig = {
   },
 };
 
-const FanDeviceType = OnOffPlugInUnitDevice.with(
-  IdentifyServer,
-  BasicInformationServer,
-  OnOffServer,
-  HomeAssistantEntityBehavior,
-  LevelControlServer.set({ config: fanLevelConfig }),
-);
-
 export function FanDevice(
   homeAssistantEntity: HomeAssistantEntityBehavior.State,
 ): EndpointType {
-  return FanDeviceType.set({ homeAssistantEntity });
+  const features: ("Auto" | "AirflowDirection" | "MultiSpeed")[] = [
+    "MultiSpeed",
+  ];
+  const attributes = homeAssistantEntity.entity.state
+    .attributes as FanDeviceAttributes;
+  const presetModes: string[] = attributes.preset_modes ?? [];
+  const supportedFeatures = attributes.supported_features ?? 0;
+
+  if (
+    supportedFeatures & FanDeviceFeature.PRESET_MODE &&
+    presetModes.indexOf("Auto") != -1
+  ) {
+    features.push("Auto");
+  }
+  if (supportedFeatures & FanDeviceFeature.DIRECTION) {
+    features.push("AirflowDirection");
+  }
+  const deviceType = Device.with(
+    IdentifyServer,
+    BasicInformationServer,
+    OnOffServer,
+    HomeAssistantEntityBehavior,
+    LevelControlServer.set({ config: fanLevelConfig }),
+    FanControlServer.with(...features),
+  );
+
+  return deviceType.set({ homeAssistantEntity });
 }
